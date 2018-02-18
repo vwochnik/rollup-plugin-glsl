5589e6e39339e4983333aedcb175655aae1e9a50
Adrian Leonhard <adrianleonhard@gmail.com>
2018-02-18 16:20:35 +0100

    Fixed test to actually test something.

--- test/index.js
+++ test/index.js
@@ -1,11 +1,14 @@
 var assert = require('assert');
 var { rollup } = require('rollup');
 var glsl = require('../');
+var fs = require('fs')
 
 process.chdir('test');
 
 function makeBundle(options) {
-	options.plugins = [glsl({})];
+	options.plugins = [glsl({
+		include: './**/*.glsl'
+	})];
 	return rollup(options);
 }
 
@@ -13,7 +16,11 @@ describe('rollup-plugin-glsl', () => {
 	it('should compress and convert imported GLSL shader', () => {
 		return makeBundle({ entry: 'fixtures/basic.js' }).then(bundle => {
 			const { code } = bundle.generate({ format: 'iife', moduleName: 'shader' });
+			console.log(bundle.generate({ format: 'iife', moduleName: 'shader' }))
+			fs.writeFileSync('./result/basic.bundle.js', code, 'utf8')
 			new Function('assert', code)(assert);
+			const expected = fs.readFileSync('./expected/basic.bundle.js')
+			assert.equal(code, expected)
 		});
 	});
 });
